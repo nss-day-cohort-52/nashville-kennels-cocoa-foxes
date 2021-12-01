6df25583aa3dec2739b6ff6e88ef4966d45bbb4c
auntviv <aviva.mondesir@gmail.com>
2021-12-01 13:59:43 -0600

    Caretaker-dropdown

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -6,12 +6,28 @@ import OwnerRepository from "../../repositories/OwnerRepository";
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
 import useResourceResolver from "../../hooks/resource/useResourceResolver";
 import "./AnimalCard.css"
-import { get } from "http";
+import { userInfo } from "os";
 
-export const Animal = ({ animal, syncAnimals,
-    showTreatmentHistory, owners }) => {
+export const Animal = 
+// the name of this component, React is a component based app, with small reusable bits of code called components
+({ animal, syncAnimals,
+    showTreatmentHistory, owners, caretakers }) => {
     const [detailsOpen, setDetailsOpen] = useState(false)
+    // declare and deconstruct an array with the useState hook
+    // you invoke useState to set up state within your application ex. const [] = useState([]) which only takes a single argument
+    // to set the initial value of the state you pass an empty array [] as the argument which is the initial value
+    // when you invoke useState it returns you an array
+    // you need to write a variable to capture the initial value (in this ex. we use the customers variable)
+    // const [customers] = useState([])
+    // the second thing the useState hook returns is a function to set the value of the variable later on in the code
+    // const [customers, setCustomers] = useState ([]) in this example we use setCustomers
+    // the second variable (setCustomers) it's just a variable that holds a fuctions who's job it is to modify state
+    // this has established a state variable to hold the state of customers from the API in our application state
     const [isEmployee, setAuth] = useState(false)
+    const [myCaretakers, setCare] = useState ([])
+    const [allCaretakers, registerCaretakers] = useState ([])
+    //thats the initial state as an argument and returns an array of two entries
+    //the second variable within the useState Hook's value is a function
     const [myOwners, setPeople] = useState([])
     const [allOwners, registerOwners] = useState([])
     const [classes, defineClasses] = useState("card animal")
@@ -24,7 +40,28 @@ export const Animal = ({ animal, syncAnimals,
         setAuth(getCurrentUser().employee)
         resolveResource(animal, animalId, AnimalRepository.get)
     }, [])
-
+    // unlike the useState hook, useEffect takes two arguments
+    // the first argument the useEffect takes is a fuction () => {},
+    // the second is always an array []
+    // useEffect's sole purpose is to run code when certain state changes
+    // it invokes the function whenever the state variable changes
+    // within the function {} we need a fetch ()
+    // when that comes back we need to convert that json encoded string into actual javascript
+    // use a .then(res => res.json())
+    // when we have the final array of customers, we want it to end up inside the first useState variable (customers)
+    // by invoking the setCustomers always to set the value of customers
+    // so you'll create the second .then, 
+    //.then((customerArray) => {setCustomers(customerArray)})
+    // what we did was
+    // the api returned a customerArray, and we use the fuction provided to us to invoke (setCustomers) the new state we want
+    // which was what was returned from the API (customerArray)
+    useEffect(() => {
+        if (caretakers) {
+            registerCaretakers(caretakers)
+        }
+    }, [caretakers])
+    // useEffect Hook tells you that your component needs to do something after that render
+    
     useEffect(() => {
         if (owners) {
             registerOwners(owners)
@@ -54,6 +91,7 @@ export const Animal = ({ animal, syncAnimals,
     }, [animalId])
 
     return (
+        //returns JSX or whatever html you want to see in the browser
         <>
             <li className={classes}>
                 <div className="card-body">
@@ -87,7 +125,24 @@ export const Animal = ({ animal, syncAnimals,
                             <span className="small">
                                 Unknown
                             </span>
+                                {
+                                     myCaretakers.length < 2
+                                     ? <select defaultValue=""
+                                         name="Caretaker"
+                                         className="form-control small"
+                                         onChange={() => {}} >
+                                         <option value="">
+                                             Select {myCaretakers.length === 1 ? "another" : "a"} caretaker
+                                         </option>
+                                         {
+                                             allCaretakers.map(
+                                                 c => 
+                                                <option key={c.id} value={c.id}>{c.name}</option>)
+                                         }
+                                     </select>
+                                     : null
 
+                                }
 
                             <h6>Owners</h6>
                             <span className="small">
@@ -136,15 +191,8 @@ export const Animal = ({ animal, syncAnimals,
                                 ? <button className="btn btn-warning mt-3 form-control small" onClick={() =>
                                     AnimalOwnerRepository
                                         .removeOwnersAndCaretakers(currentAnimal.id)
-                                        .then(() => {
-                                           AnimalRepository.delete(currentAnimal.id) 
-                                        }) // Remove animal
-                                        .then(() => {
-                                            syncAnimals()
-                                        }) // Get all animals
-                                        .then(() => {
-                                          
-                                        })
+                                        .then(() => {}) // Remove animal
+                                        .then(() => {}) // Get all animals
                                 }>Discharge</button>
                                 : ""
                         }
